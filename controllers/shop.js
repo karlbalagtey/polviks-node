e3856a035cd598a51ec66808f20fa3b9cd33fcc8
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-07-16 11:33:01 +0100

    Added cart model, separate views

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
@@ -9,6 +10,18 @@ exports.getProducts = (req, res, next) => {
             hasProducts: products.length > 0,
             activeShop: true,
             productCSS: true
+        });
+    });
+};
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product, 
+            pageTitle: product.title,
+            path: '/products'
         });
     });
 };
@@ -32,6 +45,23 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart'
     });
 };
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+
+    res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
+};
+
 
 exports.getCheckout = (req, res, next) => {
     res.render('/checkout', {
