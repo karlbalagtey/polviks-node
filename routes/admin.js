2cfbfe64355207ba6a9fe0d522c194112c743665
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-07-24 18:57:07 +0100

    initial mysql connection

--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,14 @@ const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
 router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
