0e8f6c99b1b50071a369cb4e1d7b48cd99cd6197
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-07-26 16:44:21 +0100

    Added product add, edit and delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -89,11 +89,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByPk(prodId)
-        .then(product => {
-            return product.destroy();
-        })
-        .then(result => {
+    Product.deleteById(prodId)
+        .then(() => {
             console.log('Destroyed');
             res.redirect('/admin/products');
         })
