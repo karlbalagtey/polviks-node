2ede4ba70a450425023ca160a9bdee670ebb47b9
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-07-27 10:48:18 +0100

    Adding the request driven solution

--- app.js
+++ app.js
@@ -14,43 +14,45 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById('5d3b842b47813ae86d056345')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        'mongodb+srv://kmarkb8017:Proverbs356@cluster0-c2wsd.mongodb.net/shop?retryWrites=true&w=majority'
-    )
-    .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Karl',
-                    email: 'karl.balagtey@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
+  .connect(
+    'mongodb+srv://kmarkb8017:Proverbs356@cluster0-c2wsd.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Karl',
+          email: 'karl.balagtey@gmail.com',
+          cart: {
+            items: []
+          }
         });
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
+        user.save();
+      }
     });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
